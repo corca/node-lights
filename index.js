38efb36167530877a9f381a0451263e17f0f8a2a
corca <corca006@gmail.com>
2016-01-03 18:38:54 -0700

    Added 3x send commands

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var app = express();
 app.get('/send/:id', function (req, res) {
   if (req.params.id) {
     send(req.params.id);
+    send(req.params.id);
+    send(req.params.id);
   }
   res.send("Code sent: " + req.params.id);
 });

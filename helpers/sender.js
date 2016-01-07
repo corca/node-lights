790bcb27420efb6d6bff125df17afc601780ad29
corca <corca006@gmail.com>
2016-01-06 21:06:48 -0700

    Added public files, refactored sender, included Skeleton

--- helpers/sender.js
+++ helpers/sender.js
@@ -4,11 +4,15 @@ var exec = require('child_process').exec,
 var singleCode = function (code) {
   if (code) {
     child = exec('sudo ./helpers/codesend ' + code, function (error, stdout, stderr) {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
       if (error !== null) {
         console.log('exec error: ' + error);
+        return error;
       }
+      console.log('stdout: ' + stdout + " || stderr: " + stderr);
+      return {
+        "stdout":stdout,
+        "stderr":stderr
+      };
     });
   }
 };
--- index.js
+++ index.js
@@ -2,15 +2,28 @@
 
 var express = require('express');
 var config = require('configure');
+//var jade = require('jade');
 var sender = require('./helpers/sender.js');
 
 var app = express();
 
-app.get('/send/:id', function (req, res) {
-  if (req.params.id) {
-    sender.singleCode(req.params.id);
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine','jade');
+app.set('views','./views');
+
+app.get('/', function(req,res){
+  res.render('index',config.lights);
+});
+
+app.get('/send/:id?', function (req, res) {
+  var code = req.params.id;
+  if (code === undefined) {
+    res.status(500).json({"Status":"Failed"});
+  } else {
+    //var sendStatus = sender.singleCode(code);
+    res.status(200).json({"Status":"Success"});
   }
-  res.send("Code sent: " + req.params.id);
 });
 
 app.listen(3000);

862d811010977797797796f2b68aba5fb02353f2
corca <corca006@gmail.com>
2016-01-15 09:54:13 -0700

    Improved logging on codesend errors

--- helpers/sender.js
+++ helpers/sender.js
@@ -3,11 +3,13 @@ var exec = require('child_process').exec,
 var sleep = require('sleep');
 
 function codeSend (code) {
+  console.log('Sending code: ' + code);
   child = exec('sudo ./helpers/codesend ' + code, function (error, stdout, stderr) {
     if (error !== null) {
-      console.log('exec error: ' + error);
+      console.log('Execution error: ' + error);
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
     }
-    console.log('stdout: ' + stdout + " || stderr: " + stderr);
   });
 }
 
